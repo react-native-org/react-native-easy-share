a382fe66006204c8a3beee85241b370e553d575d
Russell.H.Sun <russell.h.sun@newegg.com>
2018-12-06 19:21:41 +0800

    init project

---
+++ ios/RNEasyShare.h
@@ -0,0 +1,24 @@
+//
+//  RNCitconPay.h
+//  RNCitconPay
+//
+//  Created by cdecmac02 on 2018/10/13.
+//  Copyright © 2018年 SunnyEver0. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#if __has_include(<React/RCTBridgeModule.h>)
+#import <React/RCTBridgeModule.h>
+#else
+#import "RCTBridgeModule.h"
+#endif
+
+#import <React/RCTEventEmitter.h>
+
+@interface RNEasyShare : RCTEventEmitter <RCTBridgeModule>
+
++(void)setCitconPayToken;
++(BOOL)processOpenUrl:(UIApplication *)application openUrl:(NSURL *)url;
+
+@end
---
+++ ios/RNEasyShare.m
@@ -0,0 +1,36 @@
+//
+//  RNEasyShare.m
+//  RNEasyShare
+//
+//  Created by cdecmac02 on 2018/10/13.
+//  Copyright © 2018年 SunnyEver0. All rights reserved.
+//
+
+#import "RNEasyShare.h"
+
+@implementation RNEasyShare
+
+
+RCT_EXPORT_MODULE();
+
+- (dispatch_queue_t)methodQueue
+{
+    return dispatch_get_main_queue();
+}
+
+- (void)supportedEvents {
+//    return
+}
+
+RCT_EXPORT_METHOD(share) {
+    NSLog(@"%@",@"123");
+}
+
+- (void)dealloc
+{
+//    [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
+
+
+@end
